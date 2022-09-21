14ac79305b81a4a55410b29c7acb3cc2b335e03c
faria-r <fariacse11@gmail.com>
2022-09-21 11:58:42 +0600

    two components added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+           <h2>THis is Header components</h2> 
+        </div>
+    );
+};
+
+export default Header;
